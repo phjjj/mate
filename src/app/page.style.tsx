b6be5a6a5a12f6af802bdfcd0dba002a91d5477c
phj <aaagowns@gmail.com>
2023-07-27 00:02:12 +0900

    홈화면 css 수정
    바로 머지할겡

--- src/app/page.style.tsx
+++ src/app/page.style.tsx
@@ -1,19 +1,17 @@
+"use client";
 import { styled } from "styled-components";
 
 export const Main = styled.main`
   display: flex;
   flex-direction: column;
-  background-color: #74b9ff;
-  height: 100vh;
-  width: 100%;
-  max-width: 375px;
   margin: 0 auto;
   align-items: center;
-  gap: 600px;
-  overflow: auto;
+  gap: 500px;
+  height: 100%;
 `;
 
 export const Title = styled.main`
+  display: flex;
   color: #fff;
   font-size: 58px;
   font-weight: 400;
@@ -23,10 +21,11 @@ export const Title = styled.main`
 export const Button = styled.button`
   display: flex;
   width: 312px;
-  height: 52px;
+  min-height: 52px;
   background-color: #fff;
   border-radius: 15px;
   justify-content: center;
   align-items: center;
   border: none;
+  cursor: pointer;
 `;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 "use client";
 
 import { Main, Title, Button } from "@/app/page.style";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+  }, []);
+  return isLoading ? (
     <Main>
       <Title>MATE</Title>
       <Button>카카오톡으로 로그인하기</Button>
     </Main>
-  );
+  ) : null;
 }
