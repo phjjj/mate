357c253f06b420f978fe92e979eed6e275e45a81
phj <aaagowns@gmail.com>
2023-08-08 16:23:15 +0900

    Update route.js

--- src/app/api/users/profile/[id]/route.js
+++ src/app/api/users/profile/[id]/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import connectMongoDB from "../../_libs/mongodb";
+import connectMongoDB from "../../../../_libs/mongodb";
 import User from "../../../../_models/user";
 
 export async function GET(req, { params }) {
