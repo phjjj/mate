20d91e7800355f8f0321927001346b080fde561c
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-08-09 22:12:45 +0900

    채팅방 참여 모달 초기 셋팅

--- src/app/_components/ChatList/ChatListContainer.tsx
+++ src/app/_components/ChatList/ChatListContainer.tsx
@@ -22,11 +22,13 @@ import {
 import { BsArrowRightCircle, BsFillBookmarkFill } from "react-icons/bs";
 import { AiOutlinePlusCircle } from "react-icons/ai";
 import Link from "next/link";
-import { useSearchParams } from "next/navigation";
 import { useSession } from "next-auth/react";
+import Modal, { Backdrop } from "../Modal/Modal";
 
 export const ChatListContainer = () => {
   const { data: session } = useSession();
+  const [modal, setModal] = useState(false);
+
   console.log(session);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [chatList, setChatList] = useState([]);
@@ -34,45 +36,62 @@ export const ChatListContainer = () => {
     const res = await axios.get("/api/chats");
     setChatList(res.data.chats);
   };
+
   useEffect(() => {
     getChatList();
     setIsLoading(true);
   }, []);
 
+  const showBackdrop = modal && <Backdrop onClick={() => setModal(false)} />;
+
+  const showModal = modal && (
+    <Modal>
+      <h1>채팅방 입장 하시겠습니까?</h1>
+      <div>
+        <button onClick={() => setModal(false)}>취소</button>
+        <button>입장</button>
+      </div>
+    </Modal>
+  );
+
   return (
-    <Box>
-      <List>
-        {chatList.map((item: any, idx) => {
-          return (
-            <Item key={idx}>
-              <BsFillBookmarkFill className="mark" />
-              <ItemTitleBox>{item.title}</ItemTitleBox>
-              <ChatInfoBox>
-                <DeparturesInfoBox>
-                  <DeparturesText>{item.departures}</DeparturesText>
-                  <DeparturesTimeSpan>{item.departuresTime}</DeparturesTimeSpan>
-                </DeparturesInfoBox>
-                <BsArrowRightCircle />
-                <DestinationInfoBox>
-                  <DestinationText>{item.destination}</DestinationText>
-                  <DestinationTimeSpan>{item.destinationTime}</DestinationTimeSpan>
-                </DestinationInfoBox>
-              </ChatInfoBox>
-              <ChatInfoBox2>
-                <UserInfoBox>
-                  <UserImg />
-                  <UserNameText>{item.host.name}</UserNameText>
-                </UserInfoBox>
-                <UsersNumberSpan>{item.people}/5</UsersNumberSpan>
-              </ChatInfoBox2>
-            </Item>
-          );
-        })}
-      </List>
-      <Link href={`chat-create?id=${session?.user?.id}`}>
-        <AiOutlinePlusCircle className="button" />
-      </Link>
-    </Box>
+    <>
+      {showBackdrop}
+      {showModal}
+      <Box>
+        <List>
+          {chatList.map((item: any, idx) => {
+            return (
+              <Item key={idx} onClick={() => setModal(true)}>
+                <BsFillBookmarkFill className="mark" />
+                <ItemTitleBox>{item.title}</ItemTitleBox>
+                <ChatInfoBox>
+                  <DeparturesInfoBox>
+                    <DeparturesText>{item.departures}</DeparturesText>
+                    <DeparturesTimeSpan>{item.departuresTime}</DeparturesTimeSpan>
+                  </DeparturesInfoBox>
+                  <BsArrowRightCircle />
+                  <DestinationInfoBox>
+                    <DestinationText>{item.destination}</DestinationText>
+                    <DestinationTimeSpan>{item.destinationTime}</DestinationTimeSpan>
+                  </DestinationInfoBox>
+                </ChatInfoBox>
+                <ChatInfoBox2>
+                  <UserInfoBox>
+                    <UserImg />
+                    <UserNameText>{item.host.name}</UserNameText>
+                  </UserInfoBox>
+                  <UsersNumberSpan>{item.people}/5</UsersNumberSpan>
+                </ChatInfoBox2>
+              </Item>
+            );
+          })}
+        </List>
+        <Link href={`chat-create?id=${session?.user?.id}`}>
+          <AiOutlinePlusCircle className="button" />
+        </Link>
+      </Box>
+    </>
   );
 };
 
---
+++ src/app/_components/Modal/Modal.tsx
@@ -0,0 +1,26 @@
+import classes from "./Modal.module.css";
+import ReactDom from "react-dom";
+
+export const Backdrop = (props: any) => {
+  return <div onClick={props.onClick} className={classes.backdrop} />;
+};
+
+export const ModalOverlay = (props: any) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const Modal = (props: any) => {
+  const portalElement = document.querySelector("#overlays") as HTMLElement;
+  return (
+    <>
+      {ReactDom.createPortal(<Backdrop onClick={props.onClick} />, portalElement)}
+      {ReactDom.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
+    </>
+  );
+};
+
+export default Modal;
