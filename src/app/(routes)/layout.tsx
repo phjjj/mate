612bcbb0300b5b456f8f6ccf840958c314c4659e
phj <aaagowns@gmail.com>
2023-08-16 23:58:02 +0900

    hotfix: 모바일환경 채팅창 인풋창 확대
    모바일에서 입력하려고하니까 확대가 자동으로 되어서 설정, 근데 내가 최상위에있는 rootlayout에 하려고 했는데 이거 클라이언트컴포넌트네 수정 해야할지도

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -1,5 +1,15 @@
 import Nav from "../_components/Nav/Nav";
 
+export const metadata = {
+  title: "mate",
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+    userScalable: false,
+  },
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
 import { Main, Title } from "@/src/app/page.style";
 import LoginBtn from "@/src/app/_components/Button/LoginBtn";
 
+export const metadata = {
+  title: "mate",
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+  },
+};
 export default function Home() {
   return (
     <Main>
